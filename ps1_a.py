fab0bfb98f8c6d89c9c25679a32d07c69010c7fb
Fouzan <76258958+fouzan-abdullah@users.noreply.github.com>
2021-04-28 06:53:42 +0500

    Add files via upload

---
+++ ps1_a.py
@@ -0,0 +1,24 @@
+# house hunting with a raise: determine how long it will take you to save enough money to make the down payment
+
+annual_salary = float(input('Enter your annual salary: '))
+portion_saved = float(input('Enter portion of your salary saved as a decimal: '))
+total_cost = float(input('Enter total_cost of your dream home: '))
+semi_annual_raise = float(input('Enter semi annual salary raise as a decimal: '))
+
+current_savings = 0.0
+monthly_savings = (annual_salary / 12) * portion_saved
+r = 0.04
+investment_rate = current_savings * (r / 12)  # monthly investment rate
+portion_down_payment = 0.25 * total_cost
+num = 0
+print(num)
+while current_savings < portion_down_payment:
+    investment_rate = current_savings * (r / 12)
+    current_savings = current_savings + monthly_savings + investment_rate
+    num += 1
+    if num % 6 == 0:
+        monthly_savings = ((annual_salary + annual_salary * semi_annual_raise) / 12) * portion_saved
+print(num)
+print(investment_rate)
+print(current_savings)
+print(portion_down_payment)
